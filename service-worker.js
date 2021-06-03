ee81df4c4851b9d39bc472d9f2a3ee3d4c26e192
Xheghun <ysenruf@gmail.com>
2021-06-03 13:52:54 +0400

    xyz

--- service-worker.js
+++ service-worker.js
@@ -23,6 +23,7 @@ self.addEventListener('fetch', function (e) {
     e.respondWith(
 
         caches.match(e.request).then(function (r) {
+
             // Download the file if it is not in the cache, 
 
             return r || fetch(e.request).then(function (response) {
